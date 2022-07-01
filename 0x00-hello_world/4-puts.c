d8c0511a80eb7ad117fba74c277de50b08241ce3
Dwalker001 <clintonakpan2019@gmail.com>
2022-07-01 09:16:26 -0700

    assemblying intel syntax of C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main(void)/**Begins the code**/
+int main(void)
+/**
+*Main function Begins the code
+*/
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
