5c24015fd72da2b0013a92a3ab5b7e9735d53a80
Dwalker001 <clintonakpan2019@gmail.com>
2022-07-01 09:47:25 -0700

    I'd have to be a genius to grasp unix, right?

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,9 @@
 
 int main(void)
 /**
-*Main function Begins the code
+* main - prints exactly "Programming is like building a multilingual puzzle"
+* followed by a new line.
+* Return: Always 0 (Success)
 */
 {
 puts("\"Programming is like building a multilingual puzzle");
