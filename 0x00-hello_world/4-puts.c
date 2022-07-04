25b745550397583ae915d40fb7a85b2541b5d0da
Dwalker001 <clintonakpan2019@gmail.com>
2022-07-04 06:52:10 -0700

    P

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-
 int main(void)	/*The most important part of the program*/
 {
-	puts("\"Programming is like building a multilingual puzzle");	/* puts function in place of printf */
-	return (0);	/*Returns a success*/
+puts("\"Programming is like building a multilingual puzzle");	/* puts function in place of printf */
+return (0);/*Returns a success*/
 }
