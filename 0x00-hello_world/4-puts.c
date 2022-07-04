f2a058227098c437ba5f653baf11a49200ef25c0
Dwalker001 <clintonakpan2019@gmail.com>
2022-07-04 06:50:10 -0700

    put.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 
 int main(void)	/*The most important part of the program*/
 {
-	puts("Programming is like building a multilingual puzzle");	/* puts function in place of printf */
+	puts("\"Programming is like building a multilingual puzzle");	/* puts function in place of printf */
 	return (0);	/*Returns a success*/
 }
