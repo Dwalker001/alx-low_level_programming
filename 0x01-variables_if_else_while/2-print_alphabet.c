312ae98c8b7dc5f5fb5a485de80e3b284eb4ca8d
Dwalker001 <clintonakpan2019@gmail.com>
2022-07-04 12:27:48 -0700

    print_alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: 0 if successful
+* otherwise return a non-zero
+*/
+int main(void)
+{
+char a;
+
+a = 'a';
+while (a <= 'z')
+{
+putchar(a);
+a++;
+}
+putchar('\n');
+return (0);
+}
