eca26d73b372ed01eb6ea0e2a17f0a3c1e8ca218
Dwalker001 <clintonakpan2019@gmail.com>
2022-07-05 16:19:41 -0700

    putchar fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,6 @@
 
 /**
 * main - prints putchar
-*
 * Return : always 0
 */
 int main(void)
@@ -15,7 +14,6 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
-_putchar('r');
 _putchar('\n');
-return 0;
+ return 0;
 }
