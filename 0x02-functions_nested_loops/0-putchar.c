29dbcaf87f5be5340f9b3964017f4b0ba1c6b724
Dwalker001 <clintonakpan2019@gmail.com>
2022-07-05 16:24:42 -0700

    putchar fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
 /**
-* main - prints putchar
-* Return : always 0
+* main - The main function prints putchar.
+* Return : always 0. 
+* Returns a 1 if error.
 */
+
 int main(void)
 {
 _putchar('_');
@@ -15,5 +17,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
- return 0;
+return 0;
 }
