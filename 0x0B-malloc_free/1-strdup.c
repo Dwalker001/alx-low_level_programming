15c7e5e4a1417992cc3be34e876898a10a4746cf
Dwalker001 <clintonakpan2019@gmail.com>
2022-07-26 07:24:31 -0700

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,18 +15,19 @@
 char *_strdup(char *str)
 {
 int i, len;
-char *_strdup(char *str)
-{
-int i, len;
 char *dest;
+
 len = 0;
 if (str == NULL)
 return (NULL);
+
 while (str[len] != '\0')
 len++;
+
 dest = malloc(sizeof(char) * (len + 1));
 if (dest == NULL)
 return (NULL);
+
 for (i = 0; str[i] != '\0'; i++)
 dest[i] = str[i];
 dest[i] = '\0';
