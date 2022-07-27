50b716b5b4150227cfb585c2e23ce78d0b205064
Dwalker001 <clintonakpan2019@gmail.com>
2022-07-27 13:18:53 -0700

    Positivity of numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -21,3 +21,16 @@ if (argv[i][j] < '0' || argv[i][j] > '9')
 printf("Error\n");
 return (1);
 }
+}
+}
+if (argc == 1)
+printf("0\n");
+else
+{
+add = 0;
+for (k = 1; k < argc; k++)
+add += atoi(argv[k]);
+printf("%d\n", add);
+}
+return (0);
+}
